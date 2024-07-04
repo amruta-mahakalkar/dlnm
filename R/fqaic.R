a6853c471186b7a3d8c2837582c01ecfcaed51b4
Amruta <amrutaumakant.mahakalkar@mail.polimi.it>
2024-07-04 12:33:38 +0200

    Create fqaic.R

---
+++ R/fqaic.R
@@ -0,0 +1,7 @@
+fqaic <- 
+  function(model) {
+  loglik <- sum(dpois(model$y,model$fitted.values,log=TRUE))
+  phi <- summary(model)$dispersion
+  qaic <- -2*loglik + 2*summary(model)$df[3]*phi
+  return(qaic)
+}
